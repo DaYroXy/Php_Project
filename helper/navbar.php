a0396e481c1185e61112ff2dee3a182436d0cd00
DaYroXy <tmureckless@gmail.com>
2022-01-18 21:06:42 +0200

    add navbar

--- helper/navbar.php
+++ helper/navbar.php
@@ -1,3 +1,17 @@
 <div class="navigation">
-    <img src="../images/ShopForce.png" alt="Logo">
+    <div class="nav-Content">
+        <a href="#"><img class="Logo" src="../images/ShopForce.png" alt="Logo"></a>
+    
+        <ul class="nav-links">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Shop</a></li>
+            <li><a href="#">Cart</a><span>0</span></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    
+        <div class="Avatar">
+            <img src="../images/defaultAvatar.png" class="Avatar-Logo" alt="Avatar-Logo">
+            <a href="#">Log In</a>
+        </div>
+    </div>
 </div>
