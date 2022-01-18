e2a828df3a2e020ca60f8768c4d1fe7da5597cb2
DaYroXy <tmureckless@gmail.com>
2022-01-18 22:18:00 +0200

    modify index

--- index.php
+++ index.php
@@ -15,6 +15,21 @@
     <?php $selected="Home"; require_once "helper/navbar.php" ?>
 
     <div class="wrapper">
+        <img src="images/start.jpg" alt="Welcome-Image" class="Welcome-Image">
+        <div class="welcome-content">
+            
+            <div class="text-header">
+                <h1>Welcome to SHOP<span>FORCE</span></h1>
+            </div>
+
+            <div class="text-content">
+                <p>This website was made for the php project i hope you enjoy it thank you for visiting.</p>
+                <button>Discover More</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="home-items-content">
         
     </div>
 </body>
