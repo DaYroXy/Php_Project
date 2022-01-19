42b89192c3b30328ed199326196a640425f3fc5b
DaYroXy <tmureckless@gmail.com>
2022-01-19 04:20:58 +0200

    add shop

---
+++ shop.php
@@ -0,0 +1,85 @@
+<?php $selected="shop"; require_once "helper/navbar.php" ?>
+
+
+
+<div class="content-wrapper">
+    <div class="category-nav">
+        <ul>
+            <li><a href="#">Men's</a></li>
+            <li><a href="#">Women's</a></li>
+            <li><a href="#">Kids'</a></li>
+        
+        </ul>
+    </div>
+</div>
+
+<div class="home-items-content">
+    <div class="items-list">
+
+        <div class="card">
+            <div class="product-info">
+                <div class="product-inner-info">
+                    <h4>Green Jacket</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat </p>
+                    <div class="price-buy">
+                        <h2>$35</h2>
+                        <form action="#">
+                            <input type="submit" value="Add to cart"></input>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <img src="images/products/GreenJacket.webp" alt="">
+        </div>
+        <div class="card">
+            <div class="product-info">
+                <div class="product-inner-info">
+                    <h4>Green Jacket</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat </p>
+                    <div class="price-buy">
+                        <h2>$35</h2>
+                        <form action="#">
+                            <input type="submit" value="Add to cart"></input>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <img src="images/products/GreenJacket.webp" alt="">
+        </div>
+        <div class="card">
+            <div class="product-info">
+                <div class="product-inner-info">
+                    <h4>Green Jacket</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat </p>
+                    <div class="price-buy">
+                        <h2>$35</h2>
+                        <form action="#">
+                            <input type="submit" value="Add to cart"></input>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <img src="images/products/GreenJacket.webp" alt="">
+        </div>
+        <div class="card">
+            <div class="product-info">
+                <div class="product-inner-info">
+                    <h4>Green Jacket</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat </p>
+                    <div class="price-buy">
+                        <h2>$35</h2>
+                        <form action="#">
+                            <input type="submit" value="Add to cart"></input>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <img src="images/products/GreenJacket.webp" alt="">
+        </div>
+        
+    </div>
+    
+
+</div>
+
+<?php require_once "helper/footer.php" ?>
